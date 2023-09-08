5f1072355f4c5757fe1e9059f2ec69cb92fef45b
Nelia11 <levit.nelia@icloud.com>
2023-09-08 11:43:55 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="container">
+      <div className="row-wrap">
+        <div className="circle">1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
