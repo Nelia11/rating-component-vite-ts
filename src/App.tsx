2fcdae16ff4472c72973af45d16d844589e439a7
Nelia11 <levit.nelia@icloud.com>
2023-09-08 17:32:38 +0200

    Add desktop and mobile design for submitted state

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
 import './App.css'
+import DarkBox from './components/DarkBox/DarkBox';
 import RatingRow from './components/RatingRow/RatingRow'
 import StarredCircle from './components/StarredCircle/StarredCircle';
 import SubmitButton from './components/UI/SubmitButton/SubmitButton';
 
 function App() {
   return (
-    <div className="container">
-      <div className="row-wrap">
-        <StarredCircle />
-        <h1>How did we do?</h1>
-        <p>
-          Please let us know how we did with your support request.
-          All feedback is appreciated to help us improve our offering!
-        </p>
-        <RatingRow />
-        <SubmitButton />
-      </div>
-    </div>
+    <DarkBox>
+      <StarredCircle />
+      <h2>How did we do?</h2>
+      <p>
+        Please let us know how we did with your support request.
+        All feedback is appreciated to help us improve our offering!
+      </p>
+      <RatingRow />
+      <SubmitButton />
+    </DarkBox>
   )
 }
 
